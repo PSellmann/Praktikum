3c90007f25c061cdb82d61a39cca5376288b3583
Philipp Sellmann <philippsellmann1@gmail.com>
2024-04-30 06:25:34 +0200

    Added navigation

--- app/src/main/java/com/example/praktikum/MainActivity.kt
+++ app/src/main/java/com/example/praktikum/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.praktikum.ui.screens.App
+import com.example.navtest.ui.App
 import com.example.praktikum.ui.theme.PraktikumTheme
 
 class MainActivity : ComponentActivity() {
