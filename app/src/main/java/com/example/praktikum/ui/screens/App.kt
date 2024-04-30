2c09e29a98600a6327ca834310a56b912562a558
Philipp Sellmann <philippsellmann1@gmail.com>
2024-04-30 06:14:44 +0200

    Added main screen App.kt

---
+++ app/src/main/java/com/example/praktikum/ui/screens/App.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun App(modifier: Modifier = Modifier) {
+
+}
